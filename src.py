7cf3204bafa5940d597156ee02f1532e4d31ac92
atabaycetin <atabaycetin@hotmail.com>
2025-12-02 15:54:27 +0100

    fixed: create_link_matrix added: figure links

--- src.py
+++ src.py
@@ -1,23 +1,36 @@
 import numpy as np
 
-def create_link_matrix(links_list):
-    """
-    speed up the creation of a link matrix
-    because I can't be bothered to create a
-    for loop every time links are updated
-    """
+figure21_links = {
+    1: [2, 3, 4],
+    2: [3, 4],
+    3: [1],
+    4: [1, 3]
+}
 
+figure22_links = {
+    1: [2],
+    2: [1],
+    3: [4],
+    4: [3],
+    5: [3, 4]
+}
+
+def create_link_matrix(links_list):
     n = len(links_list)
 
     link_matrix = np.zeros((n, n), dtype=np.float64)
 
     for j, link in links_list.items():
-        # no need to deal with dangling nodes for now because this is a simple exercise
+        # I want to handle dangling nodes in the simplest way possible.
+        # Let's just distribute its vote to all pages in the web
         if len(link) == 0:
-            raise ValueError(f"Page {j} has no outgoing links (dangling node).")
-        weight = 1.0 / len(link)
-        for i in link:
-            link_matrix[i - 1, j - 1] += weight
+            weight = 1.0 / n
+            for i in range(1, n + 1):
+                link_matrix[i - 1, j - 1] = weight
+        else:
+            weight = 1.0 / len(link)
+            for i in link:
+                link_matrix[i - 1, j - 1] += weight
 
     return link_matrix
 
