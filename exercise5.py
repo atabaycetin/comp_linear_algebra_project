ff43e4316b8b018aa5e04847fa1c89b6a9f0cbfc
atabaycetin <atabaycetin@hotmail.com>
2026-01-14 01:27:59 +0100

    readded exercise 5 on paper at the beginning of the file

--- exercise5.py
+++ exercise5.py
@@ -7,21 +7,50 @@ score of a page with no backlinks is zero.
 
 
 import numpy as np
-from src import create_csr_link_matrix
+from src import create_link_matrix, create_csr_link_matrix
 
 
 if __name__ == '__main__':
+    links = {
+        1: [2, 3],
+        2: [3],
+        3: [2]
+    }
+
+    print(f"Web Structure: {links}")
+
+    link_mat = create_link_matrix(links)
+
+    print(f"Link Matrix A:\n{link_mat}")
+    print("You can see that first row is entirely zero.")
+    print("This confirms that no page votes for page 1 (page 1 has no backlinks).")
+
+    eigvals, eigvecs = np.linalg.eig(link_mat)
+
+    idx_largest_pos = np.argsort(eigvals.real)[::-1][0]
+
+    eigval_perron = eigvals[idx_largest_pos]
+    eigvec_perron = eigvecs[:, idx_largest_pos]
+
+    scaled_perronvec = eigvec_perron.real / eigvec_perron.real.sum()
+    print(f"Normalized Perron eigenvector: {scaled_perronvec}\n")
+    print("You can see that first index, the importance score of page 1, is zero.\n"
+          "Hence, we proved that the importance score of a page with no backlinks is indeed zero.")
+
+    print("\n" + "-" * 60 + "\n")
+    print("Now, let's do the exercise on the Hollins dataset")
     x0, A, _, _, _ = create_csr_link_matrix('data\\hollins.dat')
 
     in_degree = np.array(A.sum(axis=1)).flatten() # number of incoming links
 
     pages_with_no_backlinks = np.where(in_degree == 0)[0] # row indexes of pages with no backlinks
 
-    print(f"We found {len(pages_with_no_backlinks)} pages with no backlinks in our hollins web.\n")
+    print(f"We found {len(pages_with_no_backlinks)} pages with no backlinks in our Hollins web.\n")
+    print(pages_with_no_backlinks)
+    print("As you can see, those rows are entirely zero.\n")
 
     # let's check those rows
-    print(f"Sum of votes pages with no backlinks has received: {A[pages_with_no_backlinks].sum(axis=1).flatten()}")
-    print("As you can see, those rows are entirely zero.\n")
+    print(f"Sum of votes the pages with no backlinks have received: {A[pages_with_no_backlinks].sum(axis=1).flatten()}")
 
     # now we choose a page to check its importance score
     test_page = pages_with_no_backlinks[0]
